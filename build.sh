1d95b8325d6790c29c6f14761e28ff45f6d72bce
root <root@ip-172-31-3-131.ap-south-1.compute.internal>
2023-07-31 06:38:59 +0000

    likitha

---
+++ build.sh
@@ -0,0 +1,7 @@
+ #!/bin/bash
+
+      # Step 1: Navigate to the root directory of the React.js application
+      cd /root/reactjs-demo # Replace "/path/to/reactjs-demo" with the actual path to your project
+
+      # Step 2: Build the Docker image
+      docker build -t reactjs-demo-image .
